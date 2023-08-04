fbda2099f02cccbae46efbf73765ea0680bd7dc8
Valerii Maliuk <vmaliuk@bellum.ai>
2023-08-04 14:50:58 +0200

    Initial Commit

---
+++ RetryTCA/RetryTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetryTCAApp.swift
+//  RetryTCA
+//
+//  Created by Bellum on 04/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct RetryTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
